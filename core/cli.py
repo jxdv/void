7c454abb27ea95b69d4ffa5359a0574c99703ae4
jxdv <virgoj@protonmail.com>
2024-01-24 17:58:31 +0100

    add ow cli arg

--- core/cli.py
+++ core/cli.py
@@ -10,6 +10,7 @@ def parse_args():
 	parser.add_argument("-r", "--recursive", help="Path to directory which contents of will be overwritten recursively")
 	parser.add_argument("-p", "--passes", type=int, default=3, help="How many times to overwrite the file")
 	parser.add_argument("-ee", "--exclude-extensions", nargs="+", help="File extensions to ignore")
+	parser.add_argument("-ow", "--overwrite-pattern", required=True, choices=["zeroes", "ones", "random"], help="Data overwriting pattern to use")
 	args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 
 	return args
@@ -17,12 +18,16 @@ def parse_args():
 def cli():
 	args = parse_args()
 
-	if args.file:
-		shred_file(args.file, args.passes)
-
-	if args.recursive:
-		shred_directory(args.recursive, args.passes, excluded_extensions=args.exclude_extensions)
+	if args.overwrite_pattern and not (args.file or args.recursive):
+		sys.stderr.write("-ow has to be used with -f or -r\n")
+		sys.exit(1)
 
 	if args.exclude_extensions and not args.recursive:
 		sys.stderr.write("-ee arg can only be used along with -r\n")
 		sys.exit(1)
+
+	if args.file:
+		shred_file(args.file, args.passes, args.overwrite_pattern)
+
+	if args.recursive:
+		shred_directory(args.recursive, args.passes, args.overwrite_pattern, excluded_extensions=args.exclude_extensions)
